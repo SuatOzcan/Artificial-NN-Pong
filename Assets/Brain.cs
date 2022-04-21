7ab1160a103fd950a8bc60e858f7b0b85d6ba656
Suat Ozcan <41187131+SuatOzcan@users.noreply.github.com>
2022-04-21 18:27:42 +0300

    Attach paddle to the brain.

--- Assets/Brain.cs
+++ Assets/Brain.cs
@@ -15,7 +15,7 @@ public class Brain : MonoBehaviour {
 	public float numMissed = 0;
 
 	/* Inputs                     Outputs
-	 * Ball X                     Paddle velocity y 
+	 * Ball X                     Paddle velocity on y  axis.
 	 * Ball y
 	 * Ball velocity X
 	 * ball velocity y
@@ -31,9 +31,45 @@ public class Brain : MonoBehaviour {
 		ann = new ANN(6, 1, 1, 4, 0.11);
 		brb = ball.GetComponent<Rigidbody2D>();
 	}
-	
+	List<double> Run(double bx, double by,double bvx,double bvy, double px, double py,double pv,bool train)
+    {
+		List<double> inputs = new List<double>();
+		List<double> outputs = new List<double>();
+		inputs.Add(bx);
+		inputs.Add(by);
+		inputs.Add(bvx);
+		inputs.Add(bvy);
+		inputs.Add(px);
+		inputs.Add(py);
+		outputs.Add(pv);
+		if (train)
+		{
+			return (ann.Train(inputs, outputs));
+		}
+		else
+			return (ann.CalcOutput(inputs, outputs));
+
+	}
 	// Update is called once per frame
 	void Update () {
+		float posy = Mathf.Clamp(paddle.transform.position.y + (yvel * Time.deltaTime * paddleMaxSpeed), paddleMinY, paddleMaxY);
+		paddle.transform.position = new Vector3(paddle.transform.position.x, posy, paddle.transform.position.z);
 		
-	}
+		List<double> output = new List<double>();
+		int layerMask = 1 << 8;
+        RaycastHit2D hit = Physics2D.Raycast(ball.transform.position, brb.velocity, 1000, layerMask);
+
+		if (hit.collider != null && hit.collider.gameObject.tag=="backwall")
+        {
+			float dy = (hit.point.y - paddle.transform.position.y);
+			output =Run(ball.transform.position.x,ball.transform.position.y,brb.velocity.x,brb.velocity.y,
+				        paddle.transform.position.x,paddle.transform.position.y,dy,true);
+			yvel = (float)output[0];
+        }
+        else
+        {
+			yvel = 0;
+        }
+
+    }
 }
